21855526abba41a63e38aba0997e2d8ca75431f6
WifiLast <lordofdoenner@gmail.com>
2026-01-15 15:06:12 +0100

    added routing

--- MBO_Homepage/src/app/app.routes.ts
+++ MBO_Homepage/src/app/app.routes.ts
@@ -1,28 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UnternehmenComponent } from './unternehmen/unternehmen.component';
-import { ProdukteComponent } from './produkte/produkte.component';
-import { KarriereComponent } from './karriere/karriere.component';
-import { KontaktComponent } from './kontakt/kontakt.component';
-import { LegalComponent } from './legal/legal.component';
-import { ImprintComponent } from './legal/imprint/imprint.component';
-import { DatenschutzComponent } from './legal/datenschutz/datenschutz.component';
-import { AGBComponent } from './legal/agb/agb.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'unternehmen', component: UnternehmenComponent },
-  { path: 'produkte', component: ProdukteComponent },
-  { path: 'karriere', component: KarriereComponent },
-  { path: 'kontakt', component: KontaktComponent },
-  {
-    path: 'legal',
-    component: LegalComponent,
-    children: [
-      { path: 'imprint', component: ImprintComponent },
-      { path: 'datenschutz', component: DatenschutzComponent },
-      { path: 'agb', component: AGBComponent }
-    ]
-  },
-  { path: '**', redirectTo: '' }
+    { path: '', component: HomeComponent },
+    { path: 'unternehmen', component: UnternehmenComponent }
 ];
