c63aa679616ff0af1b77ba0ce600e39aed717a6b
WifiLast <lordofdoenner@gmail.com>
2026-01-20 11:41:25 +0100

    add impressum

--- MBO_Homepage/src/app/components/karriere-section/karriere-section.component.ts
+++ MBO_Homepage/src/app/components/karriere-section/karriere-section.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { SlideshowComponent } from '../slideshow/slideshow.component';
 
 @Component({
   selector: 'app-karriere-section',
-  imports: [TranslateModule],
+  imports: [TranslateModule, SlideshowComponent],
   templateUrl: './karriere-section.component.html',
   styleUrl: './karriere-section.component.css'
 })
-export class KarriereSectionComponent {}
+export class KarriereSectionComponent {
+  slides = ['/Bilder/ART/Bau/20240415_115933.jpg'];
+}
